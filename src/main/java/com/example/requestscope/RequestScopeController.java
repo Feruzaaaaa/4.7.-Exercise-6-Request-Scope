f371a262e4cbd17041e685286a16df0f92de2ed6
Feruzaaaaa <your-email@example.com>
2025-12-03 23:48:53 +0600

    first commit

---
+++ src/main/java/com/example/requestscope/RequestScopeController.java
@@ -0,0 +1,19 @@
+package com.example.requestscope;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestScopeController {
+
+    private final RequestTrace requestTrace;
+
+    public RequestScopeController(RequestTrace requestTrace) {
+        this.requestTrace = requestTrace;
+    }
+
+    @GetMapping("/request-id")
+    public String id() {
+        return "Request ID: " + requestTrace.getRequestId();
+    }
+}
